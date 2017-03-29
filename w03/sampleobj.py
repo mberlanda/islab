cf0e7ebb9b00dddd2f18a0d47dbc879dd8811316
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2017-03-29 19:03:39 +0200

    ISLAB 20170323

---
+++ w03/sampleobj.py
@@ -0,0 +1,15 @@
+class SampleOb(object):
+
+    def __init__(self):
+        return
+
+    def foo(self):
+        print 'foo'
+
+    @classmethod
+    def bar(cls):
+        print 'bar'    
+
+    @staticmethod
+    def baz(x):
+        print x    
